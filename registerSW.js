ea24a6142ee9b22d1e9a1a0e06c433f864af9713
jssrll01 <custodiojessrell07@gmail.com>
2026-09-20 11:25:43 +0800

    Fix paths for Vercel root deployment

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator){window.addEventListener('load',()=>{navigator.serviceWorker.register('/Pricee-App/sw.js',{scope:'/Pricee-App/'})})}
+if('serviceWorker' in navigator){window.addEventListener('load',()=>{navigator.serviceWorker.register('/sw.js',{scope:'/'})})}
